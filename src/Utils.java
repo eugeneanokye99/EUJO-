4b88a39fd812da5e36719982264ca9c1ef3049c7
Eugene Anokye <134564162+eugeneanokye99@users.noreply.github.com>
2024-12-02 08:36:40 -0800

    utils.java update

---
+++ src/Utils.java
@@ -0,0 +1,4 @@
+public static final String MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/myChatApp";
+public static final String DATABASE_NAME = "myChatApp";
+public static final String USERS_COLLECTION = "users";
+public static final String MESSAGES_COLLECTION = "messages";
